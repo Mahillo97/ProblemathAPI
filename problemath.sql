4a429e6953f8f28ebd7382639a65d210b83c5e91
Alejandro Mahíllo Cazorla <mahi-97@hotmail.com>
2020-04-15 13:44:57 +0200

    Updated DB

--- problemath.sql
+++ problemath.sql
@@ -26,14 +26,23 @@ CREATE TABLE `dependency` (
   `Id` int NOT NULL AUTO_INCREMENT,
   `Id_Problem` int DEFAULT NULL,
   `Id_Solu` int DEFAULT NULL,
-  `URL` varchar(100) NOT NULL,
+  `URL` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`Id`),
   KEY `FK_DEPENDENCY_PROBLEM_idx` (`Id_Problem`),
   KEY `FK_DEPENDENCY_SOLUTION_idx` (`Id_Solu`),
-  CONSTRAINT `FK_DEPENDENCY_PROBLEM` FOREIGN KEY (`Id_Problem`) REFERENCES `problem` (`Id`),
+  CONSTRAINT `FK_DEPENDENCY_PROBLEM` FOREIGN KEY (`Id_Problem`) REFERENCES `problem` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `FK_DEPENDENCY_SOLUTION` FOREIGN KEY (`Id_Solu`) REFERENCES `solution` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dependency`
+--
+
+LOCK TABLES `dependency` WRITE;
+/*!40000 ALTER TABLE `dependency` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dependency` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `problem`
@@ -44,18 +53,54 @@ DROP TABLE IF EXISTS `problem`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `problem` (
   `Id` int NOT NULL AUTO_INCREMENT,
-  `Tags` varchar(200) NOT NULL,
-  `Magacine` varchar(100) DEFAULT NULL,
-  `Tex` mediumtext NOT NULL,
-  `Proposer` varchar(50) DEFAULT NULL,
+  `Magacine` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `Tex` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Proposer` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `Dep_State` tinyint NOT NULL,
-  `URL_PDF_State` varchar(100) NOT NULL,
-  `URL_PDF_Full` varchar(100) NOT NULL,
+  `URL_PDF_State` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `URL_PDF_Full` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`Id`),
   UNIQUE KEY `URL_PDF_State_UNIQUE` (`URL_PDF_State`),
   UNIQUE KEY `URL_PDF_Full_UNIQUE` (`URL_PDF_Full`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `problem`
+--
+
+LOCK TABLES `problem` WRITE;
+/*!40000 ALTER TABLE `problem` DISABLE KEYS */;
+/*!40000 ALTER TABLE `problem` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `problem_tags`
+--
+
+DROP TABLE IF EXISTS `problem_tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `problem_tags` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `Id_Problem` int NOT NULL,
+  `Id_Tag` int NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `FK_PROBLEM_TAG_PROBLEM_idx` (`Id_Problem`),
+  KEY `FK_PROBLEM_TAG_TAG_idx` (`Id_Tag`),
+  CONSTRAINT `FK_PROBLEM_TAG_PROBLEM` FOREIGN KEY (`Id_Problem`) REFERENCES `problem` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_PROBLEM_TAG_TAG` FOREIGN KEY (`Id_Tag`) REFERENCES `tags` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `problem_tags`
+--
+
+LOCK TABLES `problem_tags` WRITE;
+/*!40000 ALTER TABLE `problem_tags` DISABLE KEYS */;
+/*!40000 ALTER TABLE `problem_tags` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `solution`
@@ -67,14 +112,46 @@ DROP TABLE IF EXISTS `solution`;
 CREATE TABLE `solution` (
   `Id` int NOT NULL AUTO_INCREMENT,
   `Id_Problem` int NOT NULL,
-  `Tex` mediumtext NOT NULL,
-  `Solver` varchar(50) DEFAULT NULL,
+  `Tex` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `Solver` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `Dep_Solu` tinyint NOT NULL,
   PRIMARY KEY (`Id`),
   KEY `FK_SOLUTION_PROBLEM_idx` (`Id_Problem`),
   CONSTRAINT `FK_SOLUTION_PROBLEM` FOREIGN KEY (`Id_Problem`) REFERENCES `problem` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `solution`
+--
+
+LOCK TABLES `solution` WRITE;
+/*!40000 ALTER TABLE `solution` DISABLE KEYS */;
+/*!40000 ALTER TABLE `solution` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tags`
+--
+
+DROP TABLE IF EXISTS `tags`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tags` (
+  `Id` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tags`
+--
+
+LOCK TABLES `tags` WRITE;
+/*!40000 ALTER TABLE `tags` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tags` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -85,12 +162,22 @@ DROP TABLE IF EXISTS `users`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
   `Id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(20) NOT NULL,
-  `password` char(40) NOT NULL,
+  `username` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` char(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`Id`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'almahill','b453eff019dfdb73916bf9dd48b780f8a7f15eb16fa118935b86f5790875e90e');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -101,4 +188,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-04-14 10:51:06
+-- Dump completed on 2020-04-15 13:44:18
