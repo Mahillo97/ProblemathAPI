3da001f461f24be0b416499a85e71c33ae1e3b0d
Alejandro <mahi-97@hotmail.com>
2020-04-15 08:25:57 +0000

    Venv updated

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/almahill/api_rest/venv"
+set -gx VIRTUAL_ENV "/home/almahill/ProblemathAPI/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
